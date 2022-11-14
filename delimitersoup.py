add9cdf8c1f5b4b016ac831152f319583dfb5994
jackson-ong-kh <84653325+jackson-ong-kh@users.noreply.github.com>
2022-11-14 21:50:23 +0800

    Create delimitersoup.py

---
+++ delimitersoup.py
@@ -0,0 +1,19 @@
+input()
+x = input()
+
+stack = []
+dictt = {'}': '{', ']': '[', ')': '('}
+oksofar = True
+
+for i, j in enumerate(x):
+    if j == ' ': continue
+    if j in dictt:
+        if dictt.get(j) == stack.pop(): 
+            continue
+        oksofar = False
+        print(f"{j} {i}")
+        break
+    else:
+        stack.append(j)
+
+if oksofar: print('ok so far')
